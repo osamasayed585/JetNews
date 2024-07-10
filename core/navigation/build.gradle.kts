d7ff26bddab0bb4c5fd1a8615e19a036c10373c1
Usama <osamasayed585@gmai.com>
2024-07-10 23:40:34 +0300

    feat: Centralize SDK versions
    Centralized compile and minimum SDK versions in the `libs.versions` block to streamline version management across modules.
    Updated Java
     and Kotlin target compatibility to version 17.

--- core/navigation/build.gradle.kts
+++ core/navigation/build.gradle.kts
@@ -5,10 +5,10 @@ plugins {
 
 android {
     namespace = "com.droidos.navigation"
-    compileSdk = 34
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
-        minSdk = 24
+        minSdk = libs.versions.minSdk.get().toInt()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -24,19 +24,22 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
 
+    // core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
+    // testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
