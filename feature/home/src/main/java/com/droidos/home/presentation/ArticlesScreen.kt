6b69c1148cd7427501c7283d6bd7a349d9bb42da
Usama <osamasayed585@gmai.com>
2024-07-19 14:12:07 +0300

    Implemented the UI for displaying individual article items in a list. Added an ElevatedCard to display article information, including an image , title, and a click listener for navigation. Updated the ArticlesScreen to use the ArticleItem composable for rendering articles.

--- feature/home/src/main/java/com/droidos/home/presentation/ArticlesScreen.kt
+++ feature/home/src/main/java/com/droidos/home/presentation/ArticlesScreen.kt
@@ -1,15 +1,21 @@
 package com.droidos.home.presentation
 
+import android.content.res.Configuration
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,6 +25,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,15 +34,19 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.droidos.common.utils.Constants.TITLE_MAX_LINES
 import com.droidos.home.R
 import com.droidos.home.domain.model.ArticleUIModel
 import com.muhammadsayed.design.components.ErrorDialog
 import com.muhammadsayed.design.components.FullScreenLoading
+import com.muhammadsayed.design.theme.JetNewsTheme
 import timber.log.Timber
 
 
 @Composable
-fun ArticlesRoute(onNavToDetails: (Int) -> Unit) {
+fun ArticlesRoute(onNavToDetails: () -> Unit) {
     val viewModel: ArticleViewModel = hiltViewModel()
     val articles = viewModel.uiState.collectAsLazyPagingItems()
     val articleState: LazyListState = rememberLazyListState()
@@ -50,7 +62,7 @@ fun ArticlesRoute(onNavToDetails: (Int) -> Unit) {
 fun ArticlesScreen(
     articles: LazyPagingItems<ArticleUIModel>,
     articleState: LazyListState,
-    onNavToDetails: (Int) -> Unit,
+    onNavToDetails: () -> Unit,
 ) {
     var showDialog by remember { mutableStateOf(true) }
 
@@ -59,14 +71,14 @@ fun ArticlesScreen(
             .fillMaxSize()
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
         state = articleState
     ) {
         items(count = articles.itemCount) {
             articles[it]?.let { article ->
-                Text(
-                    modifier = Modifier.padding(16.dp),
-                    text = article.title
+                ArticleItem(
+                    uiState = article,
+                    onCLick = onNavToDetails
                 )
             }
         }
@@ -114,7 +126,81 @@ fun ArticlesScreen(
 
 }
 
-@Preview
+@Composable
+fun ArticleItem(uiState: ArticleUIModel, onCLick: () -> Unit) {
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onCLick() },
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 4.dp
+        ),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        ),
+    ) {
+
+        AsyncImage(
+            model = ImageRequest.Builder(context = LocalContext.current)
+                .data(uiState.image)
+                .crossfade(true)
+                .build(),
+            contentDescription = stringResource(R.string.this_is_an_image_of_article),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Text(
+            modifier = Modifier.padding(16.dp),
+            text = uiState.title,
+            maxLines = TITLE_MAX_LINES,
+            style = MaterialTheme.typography.titleMedium
+
+        )
+    }
+}
+
+@Preview(
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_NORMAL
+)
+@Composable
+fun ArticleItemPreview_Light() {
+    JetNewsTheme {
+        ArticleItem(
+            uiState = ArticleUIModel(
+                title = "Netanyahu to address US Congress on 24 July",
+                image = "integer",
+                description = "Why are people in China buying salt and Why are people in China buying salt?",
+                publishedAt = "1 hour ago",
+                sourceName = "CNN"
+            ),
+            onCLick = {}
+        )
+    }
+}
+
+@Preview(
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
+)
+@Composable
+fun ArticleItemPreview_Dark() {
+    JetNewsTheme {
+
+        ArticleItem(
+            uiState = ArticleUIModel(
+                title = "Netanyahu to address US Congress on 24 July",
+                image = "integer",
+                description = "Why are people in China buying salt and Why are people in China buying salt?",
+                publishedAt = "1 hour ago",
+                sourceName = "CNN"
+            ),
+            onCLick = {}
+        )
+    }
+}
+
 @Composable
 private fun LimitedCard() {
 
